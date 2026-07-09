da6aa6c11f149cd3a32e5c68713fa226dae025f2
Vasu Sharma <253707608+vasu292005-dotcom@users.noreply.github.com>
2026-07-09 20:29:06 +0530

    Time: 8 ms (98.89%), Space: 12.5 MB (90.32%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ans = 0
+        p = 0
+        d = {}
+        for i, c in enumerate(s):
+            if c not in d or d[c] < p:
+                ans = max(ans, i - p + 1)
+            else:
+                p = d[c] + 1
+            d[c] = i
+        return ans
