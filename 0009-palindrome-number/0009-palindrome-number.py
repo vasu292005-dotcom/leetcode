4e34522fee7ff0b34c815b4cf49e41022b83ba40
Vasu Sharma <253707608+vasu292005-dotcom@users.noreply.github.com>
2026-07-10 12:54:56 +0530

    Time: 23 ms (12.05%), Space: 12.4 MB (17.18%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        x1, x2 = 0, x
+        while x > 0:
+            x1 = x1 * 10 + x % 10
+            x //= 10
+        return x1 == x2
