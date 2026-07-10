04c6a56a07d332cacfd5b481bb57068bc54eca55
Vasu Sharma <253707608+vasu292005-dotcom@users.noreply.github.com>
2026-07-10 13:12:06 +0530

    Time: 8 ms (6.52%), Space: 12.4 MB (69.23%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        d={')':'(', '}':'{', ']':'['}
+        for c1 in s:
+            if c1 in d.keys():
+                if not stack or stack.pop() != d[c1]:
+                    return False
+            else:
+                stack.append(c1)
+        if stack:
+            return False
+        return True
