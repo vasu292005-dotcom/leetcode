50f6f1f5aa9ee2a07e5b6318c953b28dcffac157
Vasu Sharma <253707608+vasu292005-dotcom@users.noreply.github.com>
2026-07-11 11:36:13 +0530

    Time: 0 ms (100%), Space: 12.5 MB (33.1%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        if not strs:
-            return ""
-        for i in range(len(strs[0])):
-            for s in strs[1:]:
-                if len(s) <= i or s[i] != strs[0][i]:
-                    return strs[0][:i]
-        return strs[0]
+        prefix = strs[0]
+        for word in strs[1:]:
+            while not word.startswith(prefix):
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        return prefix
