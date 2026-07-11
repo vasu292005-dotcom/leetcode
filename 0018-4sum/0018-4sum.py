70d6d6705323b8978130bbbd347747a188f5d87b
Vasu Sharma <253707608+vasu292005-dotcom@users.noreply.github.com>
2026-07-11 11:44:42 +0530

    Time: 535 ms (48.82%), Space: 12.3 MB (64.2%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -6,27 +6,29 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums.sort()
-        n = len(nums)
         ans = []
+        n = len(nums)
         for i in range(n - 3):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
             for j in range(i + 1, n - 2):
                 if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
-                p1, p2 = j + 1, n - 1
-                while p1 < p2:
-                    t = nums[p1] + nums[p2] + nums[i] + nums[j]
-                    if t == target:
-                        ans.append([nums[i], nums[j], nums[p1], nums[p2]])
-                        p1 += 1
-                        p2 -= 1
-                        while p1 < p2 and nums[p1] == nums[p1 - 1]:
-                            p1 += 1
-                        while p1 < p2 and nums[p2] == nums[p2 + 1]:
-                            p2 -= 1
-                    elif t < target:
-                        p1 += 1
+                left = j + 1
+                right = n - 1
+                while left < right:
+                    total = nums[i] + nums[j] + nums[left] + nums[right]
+                    if total == target:
+                        ans.append([nums[i], nums[j], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+                    elif total < target:
+                        left += 1
                     else:
-                        p2 -= 1
+                        right -= 1
         return ans
+        
