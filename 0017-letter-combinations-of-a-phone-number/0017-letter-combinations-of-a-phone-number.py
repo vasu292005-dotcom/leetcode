bc581f3728515faf6ed2bcee07a5a31a404ab85c
Vasu Sharma <253707608+vasu292005-dotcom@users.noreply.github.com>
2026-07-11 11:42:08 +0530

    Time: 0 ms (100%), Space: 12.3 MB (90.15%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -4,18 +4,17 @@ class Solution(object):
         :type digits: str
         :rtype: List[str]
         """
-        if len(digits) == 0:
+        if not digits:
             return []
-        self.d = {'2': "abc", '3': "def", '4': "ghi",
-                  '5': "jkl", '6': "mno", '7': "pqrs",
-                  '8': "tuv", '9': "wxyz"}
-        ans = []
-        self._letterCombinations(digits, "", ans)
+        phone = {
+            "2":"abc","3":"def","4":"ghi","5":"jkl",
+            "6":"mno","7":"pqrs","8":"tuv","9":"wxyz"
+        }
+        ans = [""]
+        for digit in digits:
+            temp = []
+            for prefix in ans:
+                for ch in phone[digit]:
+                    temp.append(prefix + ch)
+            ans = temp
         return ans
-
-    def _letterCombinations(self, digits, cur, ans):
-        if len(digits) == 0:
-            ans.append(cur)
-            return
-        for c in self.d[digits[0]]:
-            self._letterCombinations(digits[1:], cur + c, ans)
