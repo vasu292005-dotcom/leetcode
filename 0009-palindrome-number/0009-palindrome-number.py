e8a4580fef58a9f83f5e410521e1d7754bc785bc
Vasu Sharma <253707608+vasu292005-dotcom@users.noreply.github.com>
2026-07-11 11:34:19 +0530

    Time: 9 ms (63.63%), Space: 12.4 MB (53.98%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        if x < 0:
+        if x < 0 or (x % 10 == 0 and x != 0):
             return False
-        x1, x2 = 0, x
-        while x > 0:
-            x1 = x1 * 10 + x % 10
+        rev = 0
+        while x > rev:
+            rev = rev * 10 + x % 10
             x //= 10
-        return x1 == x2
+        return x == rev or x == rev // 10
