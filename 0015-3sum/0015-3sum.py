fcfe51bba8d2018e67ed2409399a145073a3ef70
Vasu Sharma <253707608+vasu292005-dotcom@users.noreply.github.com>
2026-07-11 11:37:10 +0530

    Time: 777 ms (36.48%), Space: 18.3 MB (78.97%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,24 +5,25 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums.sort()
-        n = len(nums)
         ans = []
+        n = len(nums)
         for i in range(n - 2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            p1, p2 = i + 1, n - 1
-            while p1 < p2:
-                t = nums[p1] + nums[p2] + nums[i]
-                if t == 0:
-                    ans.append([nums[i], nums[p1], nums[p2]])
-                    p1 += 1
-                    p2 -= 1
-                    while p1 < p2 and nums[p1] == nums[p1 - 1]:
-                        p1 += 1
-                    while p1 < p2 and nums[p2] == nums[p2 + 1]:
-                        p2 -= 1
-                elif t < 0:
-                    p1 += 1
+            left = i + 1
+            right = n - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total == 0:
+                    ans.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+                elif total < 0:
+                    left += 1
                 else:
-                    p2 -= 1
+                    right -= 1
         return ans
