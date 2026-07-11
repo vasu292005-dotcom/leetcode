ba8fba9b157f8fbdeac3a272112110591a714ac5
Vasu Sharma <253707608+vasu292005-dotcom@users.noreply.github.com>
2026-07-11 11:33:19 +0530

    Time: 7 ms (56.56%), Space: 12.4 MB (56.04%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -4,12 +4,11 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
-        nums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-        strings = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V",
-                   "IV", "I"]
-        ans = ""
-        for i in range(len(nums)):
-            while num >= nums[i]:
-                ans += strings[i]
-                num -= nums[i]
-        return ans
+        values = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+        symbols = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+        ans = []
+        for i in range(len(values)):
+            while num >= values[i]:
+                ans.append(symbols[i])
+                num -= values[i]
+        return "".join(ans)
