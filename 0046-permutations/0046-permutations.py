b4a7869ec1324f9d5dcfd30b53c11a88779d7e3f
Vasu Sharma <253707608+vasu292005-dotcom@users.noreply.github.com>
2026-07-19 13:57:58 +0530

    Time: 3 ms (54.64%), Space: 12.6 MB (23.07%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        ans = []
+        self._permute(nums, [], ans)
+        return ans
+
+    def _permute(self, nums, cur, ans):
+        if len(nums) == 0:
+            ans.append(cur[:])
+            return
+        for i, num in enumerate(nums):
+            cur.append(num)
+            self._permute(nums[:i] + nums[i + 1:], cur, ans)
+            cur.pop()
