af9e070637d82d1572e2c49f5d907186fc56178d
Vasu Sharma <253707608+vasu292005-dotcom@users.noreply.github.com>
2026-07-19 14:02:00 +0530

    Time: 0 ms (100%), Space: 12.5 MB (27.62%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n == 0:
+            return 1
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+        if n & 1:
+            return x * self.myPow(x * x, n // 2)
+        return self.myPow(x * x, n // 2)
