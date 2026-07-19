89f07a037968cf7cfa116122ab8682f76df92dcc
Vasu Sharma <253707608+vasu292005-dotcom@users.noreply.github.com>
2026-07-19 14:06:15 +0530

    Time: 37 ms (52.02%), Space: 13.2 MB (61.5%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        cur = 0
+        i, n = 0, len(nums)
+        while i <= cur:
+            if cur >= n - 1:
+                return True
+            cur = max(cur, i + nums[i])
+            i += 1
+        return False
