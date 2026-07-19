dd87210db5242ab3858d64c9c2b28fac264f21b6
Vasu Sharma <253707608+vasu292005-dotcom@users.noreply.github.com>
2026-07-19 13:33:36 +0530

    Time: 12 ms (50.35%), Space: 13.4 MB (86.62%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        l, r, ans = 0, len(height) - 1, 0
+        while l < r:
+            t = min(height[l], height[r])
+            while height[l] <= t and l < r:
+                ans += t - height[l]
+                l += 1
+            while height[r] <= t and l < r:
+                ans += t - height[r]
+                r -= 1
+        return ans
