820233f934ae2e3e134d76c9c480eb5c9aecefe7
Vasu Sharma <253707608+vasu292005-dotcom@users.noreply.github.com>
2026-08-30 09:45:59 +0530

    Time: 0 ms (100%), Space: 12.4 MB (56.66%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp=[0]*(n+1)
+        dp[0]=1
+        dp[1]=1
+        for nodes in range(2,n+1):
+            for root in range(1,nodes+1):
+                dp[nodes]+=dp[root-1]*dp[nodes-root]
+        return dp[n]
